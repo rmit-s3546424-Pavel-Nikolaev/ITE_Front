c1907e2d604b27d0b7d724e9f1dac926d3b24b4d
Benel <s3272922@student.rmit.edu.au>
2017-09-27 14:41:49 +1000

    BeBa should be committed

---
+++ public/slide/featured_jquery.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+
+$(function () {
+    //$(selector).animate(, obj, time, callback)
+    //var currentSlide = 1;
+    //setInterval(function () {
+    //    $('#slider .slides').animate({ 'margin-left': '-=720px' }, 1000);
+    //}, 3000);
+    var width = 720;
+    var speed = 1000;
+    var hold = 5000;
+    var current=1;
+    var total=4;
+    var $slider = $('#slider');
+    var $slideCountainer = $slider.find('.slides');
+    var $slides = $slideCountainer.find('.slide');
+
+    setInterval(function () {
+        $slideCountainer.animate({ 'margin-left': '-=' + width }, speed, function () {
+            current++;
+            if (current == total) {
+                current = 1;
+                $slideCountainer.css('margin-left', 0);
+            }
+        });
+    }, hold);
+});
