d33394ee2dafbd01e04e23547509eb0096009d7a
rmit-s3546424-Pavel-Nikolaev <s3546424@student.rmit.edu.au>
2017-09-30 19:38:29 +1000

    oops

--- src/js/uploader.jsx
+++ src/js/uploader.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { getDocumentS3Request, buildS3RequestData, showMsg, clearMsg } from './utils/utilities';
-// import $ from 'jquery.animate';
 import settings from './utils/config';
 
 class Uploader extends Component {
