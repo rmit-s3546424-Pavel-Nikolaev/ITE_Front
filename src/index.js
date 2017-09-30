318de41e0b0b6207f093765dace18a90db72a3b6
Dylan Lester <dylanclester@gmail.com>
2017-10-01 04:52:01 +1100

    Implement footer Please note that the odd behaviour on the other pages is completely expected. Resolving these issues requires small adjustments to the CSS.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Navbar from './js/navbar.jsx';
 import Home from './js/home';
+import Footer from './js/footer';
 import Login from './js/login';
 import Uploader from './js/uploader';
 import {clearMsg} from './js/utils/utilities';
@@ -28,6 +29,7 @@ ReactDOM.render((
                 <Route exact path='/login' component={Login}/>
                 <Route path='/upload' component={Uploader}/>
             </Switch>
+            <Footer />
         </div>
     </BrowserRouter>
 ), document.getElementById('root'));
