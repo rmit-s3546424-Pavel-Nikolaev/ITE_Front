01e09245fdf314ba46fa0b26879c6bf647112d68
rad_rmit <s3529812@student.rmit.edu.au>
2017-10-01 08:50:59 +0000

    tab in a box

--- src/js/show-tab.jsx
+++ src/js/show-tab.jsx
@@ -13,12 +13,28 @@ class ShowTab extends Component {
 render() {
     return (
         <div className='tab'>
+            <div className='tab-header'>
+                <div className='tab-title'>
+                    Tablature
+                </div>
+                <div className='tab-download-button'>
+                    Download
+                </div>
+            </div>
             <VexFlow
-                height={800}
-                width={800}
+                height={80}
+                width={300}
                 font={[ 'Arial', 10, '' ]}
                 // (x_pos, y_pos, width)
-                stave={ new Vex.Flow.Stave(0, 0, 400) }
+                stave={ new Vex.Flow.Stave(0, 0, 300) }
+                notes={ notes }
+            />
+            <VexFlow
+                height={80}
+                width={300}
+                font={[ 'Arial', 10, '' ]}
+                // (x_pos, y_pos, width)
+                stave={ new Vex.Flow.Stave(0, 0, 300) }
                 notes={ notes }
             />
         </div>
